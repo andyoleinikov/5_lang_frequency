29c98072fc68b9ec15d57e611342fbbd0ee8339b
Andrey Oleinikov <dyt321@ya.ru>
2018-04-03 13:47:46 +0300

    Solved the task

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+With this script you find 10 most frequent words in descending order from a cpecified file.
+
+# Quickstart
+
+Specify the path to a file when using this script from the command line.
+```bash
+$ python lang_frequency.py <path to file>
+```
+
+Example of script launch for 'Neuromancer' by William Gibson on Linux, Python 3.5:
+
+```bash
+
+$ python lang_frequency.py lorem_ipsum.txt
+Ten most frequent words: the, of, a, and, he, to, in, you, it, his
+
+```
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,37 @@
+import sys
+import os
+import re
+import heapq
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf-8') as text_file:
+            return text_file.read()
+    return None
 
 
 def get_most_frequent_words(text):
-    pass
+    sliced_text = [word.lower() for word in re.split('\W+', text) if word]
+    words_with_frequences = dict(Counter(sliced_text))
+    most_frequent_words = heapq.nlargest(
+        10,
+        words_with_frequences,
+        key=words_with_frequences.__getitem__
+    )
+    return most_frequent_words
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        sys.exit('No path specified')
+
+    filepath = sys.argv[1]
+    text_from_file = load_data(filepath)
+    most_frequent_words = get_most_frequent_words(text_from_file)
+
+    if most_frequent_words == []:
+        sys.exit('Empty file')
+        
+    print('Ten most frequent words: {}'.format(', '.join(most_frequent_words)))
